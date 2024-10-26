2d4427cf7af51dcb75eb527f782842abec6c5bf1
NitinSingh0 <31nitinthakur@gmail.com>
2024-10-26 12:08:57 +0530

    Setup project structure: organized main folders for components, pages, components, library, and styles.

---
+++ main/layouts/footer.php
@@ -0,0 +1,1 @@
+<!-- footer section php code here -->
---
+++ main/pages/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Leave Management Website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vaze Leave Management</title>
+    <link rel="stylesheet" href="./output.css">
+    <?php include('../layouts/header.php'); ?>
+    <!-- <link rel="stylesheet" href="" /> -->
+
+
+</head>
+
+<body class="bg-white dark:bg-black">
+    <?php include('../layouts/header.php'); ?>
+
+    <?php include('../components/Main.php'); ?>
+    <?php include('../layouts/footer.php'); ?>
+
+    <!-- <script src=""></script> -->
+</body>
+<?php include('../../library/AOS.php'); ?>
+</html>
