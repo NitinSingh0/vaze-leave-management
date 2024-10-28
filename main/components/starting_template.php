82ecfe745e77360bd8118590cddecb49cd5758f2
Omkardhadam123 <odhadam0@gmail.com>
2024-10-28 12:43:58 +0530

    kyfyfyf

--- main/components/starting_template.php
+++ main/components/starting_template.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>V. G. VAZE</title>
     <?php include('../../library/library.php'); ?>
+    <?php include('../../config/connect.php'); ?>
     <link rel="stylesheet" href="../../css/common/header.css" />
 </head>
 
@@ -22,7 +23,7 @@
     MAIN CONTENTS 
     ****************/
     -->
-    <?php include('../Components/'); ?>
+    <?php include('../Components/login.php'); ?>
 
     <!--
     /***************
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< Updated upstream
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+=======
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
+>>>>>>> Stashed changes
