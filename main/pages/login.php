7b8bdb49dbe8089549fbcdcbe55a6de7c55c0e0d
Aniket Singh <154907097+AniketSingh52@users.noreply.github.com>
2024-10-28 13:15:21 +0530

    login page

---
+++ main/pages/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V. G. VAZE</title>
+    <?php include('../../library/library.php'); ?>
+    <?php include('../../config/connect.php'); ?>
+    <link rel="stylesheet" href="../../css/common/header.css" />
+</head>
+
+<body class="bg-white dark:bg-black">
+    <?php include('../Layouts/header.php'); ?>
+    <?php include('../components/login.php'); ?>
+    <?php include('../layouts/footer.php'); ?>
+    <script src="../../js/common/header.js"></script>
+</body>
+<?php include('../../library/AOS.php'); ?>
+
+</html>
