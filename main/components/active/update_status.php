5d39105f9e9f909a65c220b00609306860f514f7
Aniket Singh <154907097+AniketSingh52@users.noreply.github.com>
2025-01-14 12:43:58 +0530

    update_status new admin panel script added

--- main/components/active/update_status.php
+++ main/components/active/update_status.php
@@ -2,9 +2,25 @@
 // update_status.php
 include("../../../config/connect.php"); // Include your database connection
 
+session_start();
+$Staff_id = $_SESSION['Staff_id'];
+//echo "console.log(" . $Staff_id . ")";
+
+if (!$Staff_id) {
+    echo "<script>alert('User not logged in.'); window.location.href='login.php';</script>";
+    exit;
+}
+
 if (isset($_POST['staff_id']) && isset($_POST['status'])) {
     $staff_id = $_POST['staff_id'];
     $newstatus= $_POST['status'];
+
+    $action = $newstatus === 'A' ? 'Activate' : 'Deactivate';
+    $action_performed_by=$Staff_id;
+    $log_query = "INSERT INTO user_status_logs (staff_id, action_performed_by, action) VALUES (?, ?, ?)";
+    $log_stmt = $conn->prepare($log_query);
+    $log_stmt->bind_param('iis', $staff_id, $action_performed_by, $action);
+    $log_stmt->execute();
 
         // Update query
         $query = "UPDATE staff SET status = '$newstatus' WHERE Staff_id = '$staff_id'";
