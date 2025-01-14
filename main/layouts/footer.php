d943e5ce75db6652f9522faccd2ffe5b24cfccc0
NitinSingh0 <31nitinthakur@gmail.com>
2025-01-14 16:08:40 +0530

    update footer styling

--- main/layouts/footer.php
+++ main/layouts/footer.php
@@ -1,3 +1,3 @@
-<footer class="bg-white text-gray-800 text-center py-3 ml-[20%] overflow-auto">
-    © 2024 Vaze Leave Management System. All Rights Reserved.
+<footer class="bg-white text-gray-800 text-center py-3 ml-[20%] overflow-auto fixed bottom-0">
+    © 2025 Vaze Leave Management System. All Rights Reserved.
 </footer>
