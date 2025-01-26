f792dca9d1f336f70a76daeab8938e7fcdc0ef1a
Aniket Singh <154907097+AniketSingh52@users.noreply.github.com>
2025-01-26 20:17:26 +0530

    connection filed fixed

--- config/connect.php
+++ config/connect.php
@@ -4,7 +4,7 @@
 define("DB_HOST", "localhost");
 define("DB_PASSWORD", "");
 define("DB_USERNAME", "root");
-define("DB_NAME", "vaze_leave_management_db_trial");
+define("DB_NAME", "vaze_leave_management_db");
 
 $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if (mysqli_connect_errno()) {
